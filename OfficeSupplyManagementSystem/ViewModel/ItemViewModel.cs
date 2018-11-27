dae1baf7a05768eb189bdfeca833a5187a39064e
Fr0thgar <jona270r@edu.easj.dk>
2018-11-27 13:21:06 +0100

    ItemViewModel
    Added ItemViewModel + iNotifyPropertyChanged

---
+++ OfficeSupplyManagementSystem/ViewModel/ItemViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeSupplyManagementSystem.Annotations;
+using OfficeSupplyManagementSystem.Model;
+
+namespace OfficeSupplyManagementSystem.ViewModel
+{
+    class ItemViewModel : INotifyPropertyChanged 
+    {
+        public ItemCatalog ItemCatalog { get; set; }
+
+        public ItemViewModel()
+        {
+            ItemCatalog = ItemCatalog.Instance;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(
+            [CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
