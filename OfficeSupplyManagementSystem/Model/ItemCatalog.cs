d63502b0d33411ff0cb9bcfc721e04803066dd43
Fr0thgar <jona270r@edu.easj.dk>
2018-11-27 13:02:54 +0100

    ItemCatalog
    Added ItemCatalog singleton

---
+++ OfficeSupplyManagementSystem/Model/ItemCatalog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeSupplyManagementSystem.Model
+{
+    class ItemCatalog
+    {
+        private static ItemCatalog _instance = new ItemCatalog();
+
+        public static ItemCatalog Instance { get => _instance; }
+
+        public ObservableCollection<Item> ItemList { get; set; }
+
+        public ItemCatalog()
+        {
+            ItemList = new ObservableCollection<Item>();
+        }
+
+        public void Add(string itemName, int itemNumber, string itemCategory, int itemAmount, bool itemStatus, int itemPrice, string itemInfo)
+        {
+            ItemList.Add(new Item(itemName, itemNumber, itemCategory, itemAmount, itemStatus, itemPrice, itemInfo));
+        }
+    }
+}
