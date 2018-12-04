35434330e1135ab550d9e1ab9e37a9861a74bfa0
J Nielsen <buhros@hotmail.com>
2018-12-04 20:36:43 +0100

    Idea
    test implementation af frame navigation

---
+++ OfficeSupplyManagementSystem/ViewModel/FrontPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using GalaSoft.MvvmLight.Command;
+using OfficeSupplyManagementSystem.View;
+
+namespace OfficeSupplyManagementSystem.ViewModel
+{
+    class FrontPageViewModel
+    {
+        public ICommand NavigateToItemCommand { get; set; }
+
+        public FrontPageViewModel()
+        {
+            NavigateToItemCommand = new RelayCommand(NavigateToItem);
+        }
+
+        public void NavigateToItem()
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(ItemPage));
+        }
+    }
+}
