3acdacb2adf36623dc7ae19eeafdd935cc698518
Fr0thgar <jona270r@edu.easj.dk>
2018-12-06 09:52:35 +0100

    Added comments

--- OfficeSupplyManagementSystem/Model/OrderCatalog.cs
+++ OfficeSupplyManagementSystem/Model/OrderCatalog.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using OfficeSupplyManagementSystem.Persistency;
@@ -10,14 +11,22 @@ namespace OfficeSupplyManagementSystem.Model
 {
     class OrderCatalog
     {
+
+        //collection class that follows the Singleton pattern, meaning there can be only one instance
+
+        //private property of the class, named _instance for clarity 
         private static OrderCatalog _instance = new OrderCatalog();
 
-       public static OrderCatalog Instance { get => _instance; }
+        //public property of the class
+       public static OrderCatalog Instance { get => _instance; } // => lambda expression
 
+        //ObervableCollection of Orders with automatic get; set;
        public ObservableCollection<Order> OrderList { get; set; }
 
+        //constructor that take 0 parameters
         public OrderCatalog()
         {
+            //initiates the ObservableCollection of items
             OrderList = new ObservableCollection<Order>();
             LoadOrderAsync();
         }
@@ -37,6 +46,7 @@ namespace OfficeSupplyManagementSystem.Model
             }
             else
             {
+                //TODO add deafault orders to the collection for testing purposes
 //                OrderList.Add(new Order(514785, "Sanitation", 250, "gertsvej 12", , "Jensen"));
 
                 SaveFile();
@@ -48,6 +58,7 @@ namespace OfficeSupplyManagementSystem.Model
             PersistencyService.SaveCollectionAsJsonAsync<ObservableCollection<Order>>(OrderList);
         }
 
+        //Add method that corresponds to hte item class constructor, the names doesnt have to match, only for clarity
         public void Add(int orderNumber, string orderType, int orderTotalPrice,
             string orderDeliveryAddress, DateTime orderDate, string orderAccount)
         {
