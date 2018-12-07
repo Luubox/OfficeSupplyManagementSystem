659e4f0d493834382e73a78920bd2d89f9242aab
Fr0thgar <jona270r@edu.easj.dk>
2018-12-07 12:50:22 +0100

    Added OrderStatus

--- OfficeSupplyManagementSystem/Model/OrderCatalog.cs
+++ OfficeSupplyManagementSystem/Model/OrderCatalog.cs
@@ -18,10 +18,10 @@ namespace OfficeSupplyManagementSystem.Model
         private static OrderCatalog _instance = new OrderCatalog();
 
         //public property of the class
-       public static OrderCatalog Instance { get => _instance; } // => lambda expression
+        public static OrderCatalog Instance { get => _instance; } // => lambda expression
 
         //ObervableCollection of Orders with automatic get; set;
-       public ObservableCollection<Order> OrderList { get; set; }
+        public ObservableCollection<Order> OrderList { get; set; }
 
         //constructor that take 0 parameters
         public OrderCatalog()
@@ -47,7 +47,7 @@ namespace OfficeSupplyManagementSystem.Model
             else
             {
                 //TODO add deafault orders to the collection for testing purposes
-                OrderList.Add(new Order(514785, "Sanitation", 250, "gertsvej 12", new DateTime() , "Jensen"));
+                OrderList.Add(new Order(514785, "Sanitation", 250, "gertsvej 12", new DateTime(), "Jensen", "Test Status"));
 
                 SaveFile();
             }
@@ -60,9 +60,9 @@ namespace OfficeSupplyManagementSystem.Model
 
         //Add method that corresponds to hte item class constructor, the names doesnt have to match, only for clarity
         public void Add(int orderNumber, string orderType, int orderTotalPrice,
-            string orderDeliveryAddress, DateTime orderDate, string orderAccount)
+            string orderDeliveryAddress, DateTime orderDate, string orderAccount, string orderStatus)
         {
-            Order newOrder = new Order(orderNumber, orderType, orderTotalPrice, orderDeliveryAddress, orderDate, orderAccount);
+            Order newOrder = new Order(orderNumber, orderType, orderTotalPrice, orderDeliveryAddress, orderDate, orderAccount, orderStatus);
             OrderList.Add(newOrder);
             PersistencyService.SaveCollectionAsJsonAsync<ObservableCollection<Order>>(OrderList);
         }
