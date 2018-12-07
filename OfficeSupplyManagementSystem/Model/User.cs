e4d4923d7169f302bc3c30dbb1c6af1728f80edf
J Nielsen <buhros@hotmail.com>
2018-12-07 12:50:08 +0100

    login

---
+++ OfficeSupplyManagementSystem/Model/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeSupplyManagementSystem.Model
+{
+    class User
+    {
+        private string _username;
+        private string _password;
+
+        public string Username { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
+
+        public User()
+        {
+            
+        }
+
+        public User(string username, string password)
+        {
+            _username = username;
+            _password = password;
+        }
+    }
+}
