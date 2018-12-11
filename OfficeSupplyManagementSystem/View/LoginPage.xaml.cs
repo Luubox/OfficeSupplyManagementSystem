f4459bc2a7d445e87933f972b69244d99b55d5d7
NKragh <39466838+NKragh@users.noreply.github.com>
2018-12-11 12:17:57 +0100

    Testing login

--- OfficeSupplyManagementSystem/View/LoginPage.xaml.cs
+++ OfficeSupplyManagementSystem/View/LoginPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using OfficeSupplyManagementSystem.ViewModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,14 +25,28 @@ namespace OfficeSupplyManagementSystem.View
     /// </summary>
     public sealed partial class LoginPage : Page
     {
+        private LoginViewModel loginViewModel;
         public LoginPage()
         {
             this.InitializeComponent();
+
+            loginViewModel = new LoginViewModel();
         }
 
         private void PasswordBox_OnKeyDown(object sender, KeyRoutedEventArgs e)
         {
             throw new NotImplementedException();
+        }
+
+        private void PasswordBox_OnKeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            Debug.WriteLine(e.Key);
+            if (e.Key.ToString() == "Enter")
+            {
+                Debug.WriteLine(loginViewModel.TempUser.Username);
+                Debug.WriteLine(loginViewModel.TempUser.Password);
+                loginViewModel.CheckCredentials();
+            }
         }
     }
 }
